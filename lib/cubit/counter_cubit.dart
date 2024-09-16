f8d2f3b48dca58b34c469c6bfb6ba6b633b83efd
Mohammed Hashem Bdier <mhb20033@gmail.com>
2024-09-16 13:47:00 +0300

    cubit and states

---
+++ lib/cubit/counter_cubit.dart
@@ -0,0 +1,6 @@
+import 'package:basketball_points_counter/cubit/counter_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CounterCubit extends Cubit<CounterState> {
+  CounterCubit(super.initialState);
+}
---
+++ lib/cubit/counter_state.dart
@@ -0,0 +1,5 @@
+abstract class CounterState {}
+
+class CounterTeamAIncrementState extends CounterState {}
+
+class CounterTeamBIncrementState extends CounterState {}
